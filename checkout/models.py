bbdcfcd73b0068f4c53527326fac47845ed7a32b
RDGrover <rossgrover95@hotmail.co.uk>
2021-08-23 02:03:07 +0000

    add signals and forms'

--- checkout/models.py
+++ checkout/models.py
@@ -1,7 +1,9 @@
 import uuid
+
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+
 from products.models import Product
 
 
@@ -22,9 +24,11 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
+
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
+
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
@@ -34,23 +38,28 @@ class Order(models.Model):
         self.save()
 
     def save(self, *args, **kwargs):
+
         if not self.order_number:
             self.order_number = self._generate_order_number
         super().save(*args, **kwargs)
 
     def __str__(self):
+
         return self.order_number
 
 
 class OrderLineItem(models.Model):
+
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
+
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
-  
+
     def __str__(self):
+
         return f'SKU {self.product.sku} on order {self.order.order_number}'
