ee1dcd80aed770704a2fbdd07418874c6cdfbbe7
RDGrover <rossgrover95@hotmail.co.uk>
2021-08-27 18:10:01 +0000

    create blog app, views, urls, templates and models

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import BlogPost, Comments
+# Create your views here.
+
+
+def blog_list(request):
+    posts = BlogPost.objects.all().limit(5)
+
+    return render(request, 'blog/blog.html', {"posts": posts})
